b2fe8c78884530d0505676e6929aea6cf88cfaab
lnblanke <50052688+lnblanke@users.noreply.github.com>
2020-02-16 18:33:11 +0800

    Update 0216

--- zh-fan/main.js
+++ zh-fan/main.js
@@ -17,30 +17,30 @@ var st = document.getElementById ( "sche-title" ) ;
 var sd = document.getElementById ( "sche-desp" ) ;
 var ft = document.getElementById ( "foot" ) ;
 
-function translate_zh () 
+function translate_fan () 
 {
-	gcs.innerHTML = "<link rel = 'stylesheet' type = 'text/css' href = 'zh-cn/home.css' >"
-	tt.innerHTML = "首页"
+	gcs.innerHTML = "<link rel = 'stylesheet' type = 'text/css' href = 'zh-jyut/home.css' >"
+	tt.innerHTML = "主頁"
 	hi.innerHTML = "<a href = 'main.html'> <img src = 'repository/title.jpg' > </a>"
-	nsc.innerHTML = "搜索" ;
-	np.innerHTML = "练习" ;
-	ne.innerHTML = "实验" ;
+	nsc.innerHTML = "搜尋" ;
+	np.innerHTML = "練習" ;
+	ne.innerHTML = "試驗" ;
 	ni.innerHTML = "信息" ;
-	ns.innerHTML = "日程表" ;
-	na.innerHTML = "关于" ;
-	pd.innerHTML = "有题目不会做？来找下答案！" ;
-	pb.innerHTML = "搜索" ;
-	et.innerHTML = "实验" ;
-	ed.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;本网站提供线上实验室功能，帮助学生足不出户便可以完成在实验室才能完成的物理和化学\
-	实验，让学生能够顺利通过有关的实验考试。" ;
+	ns.innerHTML = "日程" ;
+	na.innerHTML = "關於" ;
+	pd.innerHTML = "有題目不會做？來找下答案！" ;
+	pb.innerHTML = "影像" ;
+	et.innerHTML = "試驗" ;
+	ed.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;本網站提供線上實驗室功能，幫助學生足不出戶便可以完成在實驗室才能完成的物理和\
+	化學實驗，讓學生能夠順利通過相關的實驗測試。" ;
 	it.innerHTML = "信息" ;
-	id.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;本网站提供关于A-level、AP、SAT和ACT的备考资料，包括考试时间、官方指南、考生须知等\
-	帮助考生在考试前掌握所需的资料并做好准备。" ;
-	st.innerHTML = "日程表" ;
-	sd.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp; 本网站提供为考生准备的考试日程表，其中囊括了国际考试注册截止日期和考试时间，帮\
-	助考生合理计划时间并充分准备考试，避免错过重要的考试。" 
-	ft.innerHTML = "语言：" ;
+	id.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;本網站提供關於A-level、AP、SAT和ACT的備考資料，包括考試時間、官方指南、考生須\
+	知等，幫助考生在考試前掌握所需的資料並做好充足準備。" ;
+	st.innerHTML = "日程" ;
+	sd.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;本網站提供為考生準備的考試日程表，其中囊括了國際考試註冊截止日期和考試時間，\
+	幫助考生合理規劃安排時間，並充分準備考試，避免錯過重要的考試。" 
+	ft.innerHTML = "語言：" ;
 
 
-	document.cookie = "lang = zh; expires=Fri, 31 Dec 9999 23:59:59 GMT" ;
+	document.cookie = "lang = zh-fan; expires=Fri, 31 Dec 9999 23:59:59 GMT" ;
 }
