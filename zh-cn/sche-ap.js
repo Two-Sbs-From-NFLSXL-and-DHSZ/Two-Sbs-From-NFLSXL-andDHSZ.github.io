1410d790103f050c62f176f3e274b9a54cd2bf78
lnblanke <50052688+lnblanke@users.noreply.github.com>
2020-02-28 19:05:06 +0800

    Test Update

--- zh-cn/sche-ap.js
+++ zh-cn/sche-ap.js
@@ -37,5 +37,5 @@ function translate_zh ()
 
 	ft.innerHTML = "语言：" ;
 	
-	document.cookie = "lang = zh; expires=Fri, 31 Dec 9999 23:59:59 GMT" ;
+	document.cookie = "lang = zh; expires=Fri, 31 Dec 9999 23:59:59 GMT; path = /" ;
 }
