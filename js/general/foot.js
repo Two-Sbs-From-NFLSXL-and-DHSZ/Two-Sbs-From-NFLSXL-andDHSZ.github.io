1bc6a7fed614d7e28cff63bd131543861aa6cf65
gitkit <tigerfu999@163.com>
2020-03-02 09:16:09 +0800

    Update 0302
    Update:
    css\schedule\
    en\schedule\
    js\general\
    js\schedule\

---
+++ js/general/foot.js
@@ -0,0 +1,54 @@
+function ft ()
+{
+	document.write ( "<div class = 'foot-nav' >" ) ;
+	
+	document.write ( "<div id = 'foot-nav-search' >" ) ;
+	document.write ( "<a href = '../search.html' >" ) ;
+	document.write ( "Search" ) ;
+	document.write ( "</a>" ) ;
+	document.write ( "</div>" ) ;
+
+	document.write ( "<p> | </p>" ) ;
+
+	document.write ( "<div id = 'foot-nav-prac' >" ) ;
+	document.write ( "<a href = '../practice.html' >" ) ;
+	document.write ( "Practice" ) ;
+	document.write ( "</a>" ) ;
+	document.write ( "</div>" ) ;
+
+	document.write ( "<p> | </p>" ) ;
+
+	document.write ( "<div id = 'foot-nav-exp' >" ) ;
+	document.write ( "<a href = '../experiment.html' >" ) ;
+	document.write ( "Experiment" ) ;
+	document.write ( "</a>" ) ;
+	document.write ( "</div>" ) ;
+
+	document.write ( "<p> | </p>" ) ;
+
+	document.write ( "<div id = 'foot-nav-info'>" ) ;
+	document.write ( "<a href = '../info.html' >" ) ;
+	document.write ( "Information" ) ;
+	document.write ( "</a>" ) ;
+	document.write ( "</div>" ) ;
+
+	document.write ( "<p> | </p>" ) ;
+
+	document.write ( "<div id = 'foot-nav-sche' >" ) ;
+	document.write ( "<a href = '../schedule.html' >" ) ;
+	document.write ( "Schedule" ) ;
+	document.write ( "</a>" ) ;
+	document.write ( "</div>" ) ;
+
+	document.write ( "<p> | </p>" ) ;
+
+	document.write ( "<div id = 'foot-nav-about' >" ) ;
+	document.write ( "<a href = '../about.html' >" ) ;
+	document.write ( "About" ) ;
+	document.write ( "</a>" ) ;
+	document.write ( "</div>" ) ;
+	
+	document.write ( "</div>" ) ;
+}
+
+ft () ;
---
+++ js/general/nav.js
@@ -0,0 +1,22 @@
+function nav () 
+{
+	document.write ( "<div id = 'header' > " ) ;
+    document.write ( "<a href = '../main.html'> <img src = '../../repository/title.jpg' > </a> " ) ;
+    document.write ( "</div>" ) ;
+
+    document.write ( "<nav id = 'Navigation' > " ) ;
+    
+    document.write ( "<a href = '../search.html' id = 'nav-search' class = 'Navi' > Search </a> " ) ;
+    
+    document.write ( "<a href = '../prac.html' id = 'nav-prac' class = 'Navi' > Practice </a> " ) ;
+
+	document.write ( "<a href = '../experiment.html' id = 'nav-exp' class = 'Navi' > Experiment </a> " ) ;
+
+	document.write ( "<a href = '../info.html' id = 'nav-info' class = 'Navi' > Information </a> " ) ;
+
+	document.write ( "<a href = '../schedule.html' id = 'nav-sche' class = 'Navi hovered' > Schedule </a> " ) ;
+
+	document.write ( "<a href = '../about.html' id = 'nav-about' class = 'Navi' > About Us </a> " ) ;
+
+    document.write ( "</nav>" ) ; 
+}
