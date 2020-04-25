31ff1584685e15e7132172d17afb63f8e34eddee
lnblanke <50052688+lnblanke@users.noreply.github.com>
2020-04-25 20:52:37 +0800

    Update 0425
    Fix a bug in main-in.js

--- js/main-in.js
+++ js/main-in.js
@@ -8,7 +8,7 @@ window.onscroll = function ()
 	if ( window.screen.width > window.screen.height )
 	{
 		for ( i = 0 ; i < idlist.length ; i++ )
-			if ( document.documentElement.scrollTop > x( idlist [ i ].offsetTop + document.querySelector ( ".model").offsetTop - idlist [ i ].offsetHeight ) )
+			if ( document.documentElement.scrollTop > ( idlist [ i ].offsetTop + document.querySelector ( ".model").offsetTop - idlist [ i ].offsetHeight ) )
 			{
 				idlist [ i ].style.animation = "fade-in 0.5s ease-in" ;
 				idlist [ i ].style.opacity = 1 ;
